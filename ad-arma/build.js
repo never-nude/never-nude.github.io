355c22cb28eb7b920c54cef0b77a4fac2781ced2
Mike <YOUR_EMAIL_HERE>
2026-02-19 12:53:25 -0500

    Ad Arma v2 AUGUSTUS 20260219-111724 (PLAY_MOVEMENT_V0)

--- ad-arma/build.js
+++ ad-arma/build.js
@@ -1,3 +1,2 @@
-// Ad Arma v2 build stamp
-window.AD_ARMA_BUILD_ID = "20260219-012640";
-window.AD_ARMA_PROJECT  = "Ad Arma v2";
+window.AD_ARMA_BUILD_ID = "20260219-111724";
+window.AD_ARMA_PROJECT = "Ad Arma v2";
